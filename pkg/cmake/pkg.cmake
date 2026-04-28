94a66483c766afcad8e297458b605fe9428a0e8d
yuliyavroma-spec <yuliyavroma@gmail.com>
2026-04-28 18:49:56 +0300

    update hunter package pkg (local)

--- pkg/cmake/pkg.cmake
+++ pkg/cmake/pkg.cmake
@@ -1,8 +1,1 @@
-hunter_add_version(
-    PACKAGE pkg
-    VERSION "1.0.0"
-    URL "https://github.com/yuliayvroma/lab07/archive/refs/heads/master.tar.gz"
-    SHA1 ""
-)
-
 hunter_config(pkg VERSION 1.0.0)
